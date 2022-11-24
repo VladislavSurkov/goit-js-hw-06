f3dfac1491a7ebcfe15725790a1d89cbcc7a0e24
VladislavSurkov <fvhabjrega@gmail.com>
2022-11-24 21:50:41 +0200

    hm06

---
+++ js/task-02.js
@@ -0,0 +1,24 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const listEl = document.querySelector("#ingredients");
+
+const makeIngredientsOptions = options => {
+  return options.map((option) => {
+    const ingredientItemEl = document.createElement("li");
+    ingredientItemEl.textContent = option;
+    ingredientItemEl.classList.add("item");
+
+    return ingredientItemEl;
+  });
+}
+
+const elements = makeIngredientsOptions(ingredients);
+listEl.append(...elements);
+console.log(listEl);
+
---
+++ js/task-03.js
@@ -0,0 +1,32 @@
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const galleryEl = document.querySelector(".gallery");
+
+const makeImages = (options) => {
+  return options.map(
+    (option) =>
+      `<li><img src= ${option.url} alt= ${option.alt} width= 300></li>`
+  );
+  
+}
+
+const arr = makeImages(images)
+galleryEl.insertAdjacentHTML("beforeend", arr);
+
+galleryEl.style = "list-style: none";
+
+console.log(galleryEl);
+
